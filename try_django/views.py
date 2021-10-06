9a0a6ba81a61020dc9525886e7637c7619073db4
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-07 00:12:39 +0800

    Access article models and write it to HTML

--- try_django/views.py
+++ try_django/views.py
@@ -3,6 +3,9 @@ To render HTML web page
 """
 
 from django.http import HttpResponse
+from random import randint
+
+from articles.models import Article
 
 
 def home_view(request):
@@ -12,9 +15,17 @@ def home_view(request):
     @return: Returns HTML as a response
     """
 
-    name = "Alfian"
-    html_string = """
-    <h1>Hello {}</h1>
-    """.format(name)
+    random_id = randint(1, 3)
+    article_obj = Article.objects.get(id=2)
+
+    h1_string = """
+    <h1>{}</h1>
+    """.format(article_obj.title)
+
+    p_string = """
+    <p>{}</p>
+    """.format(article_obj.title)
+
+    html_string = h1_string + p_string
 
     return HttpResponse(html_string)
