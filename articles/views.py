b0636a5dc95452ee3c26faa16a75667a88c16f65
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-10 23:12:31 +0800

    Handle dynamic URL routing

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+
+def article_detail_view(request, id=None):
+    """
+    Render article's detail
+    @param request: Django's request
+    @param id: Article's ID
+    @return: Returns detail.html as a response
+    """
+
+    article_obj = None
+
+    if id is not None:
+        article_obj = Article.objects.get(id=id)
+
+    context = {
+        "object": article_obj
+    }
+
+    return render(request, "articles/detail.html", context=context)
