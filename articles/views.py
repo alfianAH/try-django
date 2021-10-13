434a30c6fe76c56cf8903965f6c35af1dbecc4c6
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-10-13 16:09:18 +0800

    Add search form

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,36 @@ from django.shortcuts import render
 from .models import Article
 
 
+def article_search_view(request):
+    """
+    Render article search view
+    @param request:
+    @return:
+    """
+
+    # Get the input dictionary
+    query_dict = request.GET
+
+    # Parse the query dictionary
+    try:
+        # Search by id
+        query = int(query_dict.get("q"))
+    except ValueError:
+        query = None
+
+    article_obj = None
+
+    if query is not None:
+        # Get the article by id
+        article_obj = Article.objects.get(id=query)
+
+    context = {
+        "object": article_obj
+    }
+
+    return render(request, "articles/search.html", context=context)
+
+
 def article_detail_view(request, id=None):
     """
     Render article's detail
