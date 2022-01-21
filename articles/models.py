cb5c3a205493c4d124208b40967e563513e10f78
alfianAH <alfian.hamdani04@gmail.com>
2022-01-21 17:34:18 +0800

    Make and generate slug to make it unique

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.utils import timezone
-from django.utils.text import slugify
+from django.db.models.signals import pre_save, post_save
+from .utils import slugify_instance_title
 
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=60)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
 
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -14,6 +14,20 @@ class Article(models.Model):
     publish = models.DateField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    if instance.slug is None:
+        slugify_instance_title(instance)
+
+
+def article_post_save(sender, instance, created, *args, **kwargs):
+    print('post_save')
+    if created:
+        slugify_instance_title(instance, save=True)
+
+
+pre_save.connect(article_pre_save, sender=Article)
+post_save.connect(article_post_save, sender=Article)
---
+++ articles/utils.py
@@ -0,0 +1,32 @@
+from django.utils.text import slugify
+import random
+
+
+def slugify_instance_title(instance, save=False, new_slug=None):
+    """
+    Slugify title
+    """
+    
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+
+    # Query the slug
+    Klass = instance.__class__  # Get the class
+    query = Klass.objects.filter(slug=slug).exclude(id=instance.id)
+
+    if query.exists():
+        # Randomize int to prevent bad slug
+        random_int = random.randint(300_000, 500_000)
+        # Auto generate new slug
+        slug = '{}-{}'.format(slug, random_int)
+
+        # Recursion until there is no query
+        return slugify_instance_title(instance, save=save, new_slug=slug)
+
+    instance.slug = slug  # Set the slug
+
+    if save: 
+        instance.save()
+    return instance
