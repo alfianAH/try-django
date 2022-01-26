1e1e388360bbdbe316c923387e05b0420c276241
alfianAH <alfian.hamdani04@gmail.com>
2022-01-26 14:43:52 +0800

    Add user foreign key in artimodel

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from .utils import slugify_instance_title
+
+
+User = settings.AUTH_USER_MODEL
 
 
 class ArticleQuerySet(models.QuerySet):
@@ -35,6 +39,7 @@ class ArticleManager(models.Manager):
 
 # Create your models here.
 class Article(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=60)
     slug = models.SlugField(unique=True, blank=True, null=True)
 
